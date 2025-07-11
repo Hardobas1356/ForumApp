2a33ee792e3dc089d069bb3ac4c8e67396539129
Hardobas <kalinhabacha@gmail.com>
2025-07-11 12:41:31 +0300

    Added Admin area and route

---
+++ ForumApp/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,19 @@
+﻿using ForumApp.Services.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ForumApp.Web.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class DashboardController : Controller
+{
+    private readonly IBoardService boardService;
+    public DashboardController(IBoardService boardService)
+    {
+        this.boardService = boardService;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
--- ForumApp/Program.cs
+++ ForumApp/Program.cs
@@ -88,7 +88,11 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
+    pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}");
+app.MapControllerRoute(
+    name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
 app.MapRazorPages();
 
 app.Run();
